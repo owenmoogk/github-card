b481e1246f286699881e71751022413ef9485603
owenmoogk <owenmoogk@gmail.com>
2021-06-03 14:10:23 -0400

    added github card :card_index:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import GithubCard from './GithubCard'
+
 export default function App() {
 	return (
 		<div className="App">
+			<GithubCard username="Tarptaeya" repo='repo-card' />
 		</div>
 	);
 }
