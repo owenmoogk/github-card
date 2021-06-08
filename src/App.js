323f01f348938128cc595d4687f728c237f7eeff
owenmoogk <owenmoogk@gmail.com>
2021-06-08 15:14:35 -0400

    added user input :musical_keyboard:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import GithubCard from './GithubCard'
+import UserPage from './UserPage'
+import react, { useState } from 'react'
+import './styles.css'
 
 export default function App() {
+
+	const [username, setUsername] = useState()
+	const [repo, setRepo] = useState()
+
+	function submit(){
+		setUsername(document.getElementById('usernameInput').value)
+		setRepo(document.getElementById('repoInput').value)
+	}
+
 	return (
 		<div className="App">
-			<GithubCard username="Tarptaeya" repo='repo-card' />
+			<h1>Github Cards (React style)</h1>
+			<div id='inputGroup'>
+				<input placeholder='User' id='usernameInput' />
+				<input placeholder='Repository' id='repoInput' />
+				<button onClick={() => submit()}>Submit</button>
+			</div>
+			<br/>
+			<GithubCard username={username} repo={repo} />
+			
 		</div>
 	);
 }
