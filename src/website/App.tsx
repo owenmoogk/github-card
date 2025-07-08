597506289d0f42ebac7d8a2ba7b5586b182657b2
Owen Moogk <owenmoogk@gmail.com>
2025-07-07 21:10:48 -0400

    Update documentation page

--- src/website/App.tsx
+++ src/website/App.tsx
@@ -62,6 +62,16 @@ export default function App() {
       </div>
       <br />
       {cardProps && <GithubCard {...cardProps} />}
+      <p style={{ textAlign: 'center' }}>
+        Download the package with{' '}
+        <pre style={{ display: 'inline' }}>npm install github-repo-card</pre> or{' '}
+        <pre style={{ display: 'inline' }}>yarn add github-repo-card.</pre>
+      </p>
+      <p style={{ textAlign: 'center' }}>
+        For more information, check it out on{' '}
+        <a href="https://github.com/owenmoogk/github-repo-card">GitHub</a> or{' '}
+        <a href="https://npmjs.com/github-repo-card">NPM</a>.
+      </p>
     </div>
   );
 }
